3f68dcbfa3e69ad56bd5bb0b3844df1c2916e9cb
Madhari Tarunsai <saitarunsai04062002@gmail.com>
2026-07-17 14:07:00 +0530

    added

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request, status
+from fastapi import FastAPI
 from src.Users.Routers import user_router
 from src.Tasks.Routers import TaskRouter
 from src.Utils.db import Base, engine
